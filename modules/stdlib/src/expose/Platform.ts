05ad6a5606b92fda63d78f9044d59105782da9f3
Delusoire <deluso7re@outlook.com>
2024-07-03 21:33:12 +0100

    fix(scripts): vars with spaces being interpreted as multiple arguments

--- modules/stdlib/src/expose/Platform.ts
+++ modules/stdlib/src/expose/Platform.ts
@@ -25,7 +25,7 @@ transformer<Platform>(
 	{
 		then: ($: Platform) => {
 			Platform = $;
-			Cosmos = $.getRegistry().resolve(Symbol.for("Cosmos"));
+			Cosmos = $.getRegistry()._map.get(Symbol.for("Cosmos"));
 		},
 		glob: /^\/xpui\.js/,
 	},
