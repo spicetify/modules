edbfdd256b94126615824ab937a0ef08ec1f6dc3
Delusoire <deluso7re@outlook.com>
2024-07-13 02:23:18 +0100

    fix

--- modules/stdlib/src/webpack/ReactComponents.xpui.ts
+++ modules/stdlib/src/webpack/ReactComponents.xpui.ts
@@ -5,7 +5,13 @@
 
 import { capitalize } from "../../deps.ts";
 import { Platform } from "../expose/Platform.ts";
-import { chunks, exportedForwardRefs, exportedFunctions, exportedMemos, require } from "./index.ts";
+import {
+	chunks,
+	exportedForwardRefs,
+	exportedFunctions,
+	exportedMemos,
+	require,
+} from "./index.ts";
 import { findBy } from "/hooks/util.ts";
 import { React } from "../expose/React.ts";
 
@@ -28,7 +34,9 @@ export const Menus: any = Object.fromEntries(
 	}),
 );
 
-const [ContextMenuModuleID] = chunks.find(([_, v]) => v.toString().includes("toggleContextMenu"))!;
+const [ContextMenuModuleID] = chunks.find(([_, v]) =>
+	v.toString().includes("toggleContextMenu")
+)!;
 const [playlistMenuChunkID] = chunks.find(
 	([, v]) =>
 		v.toString().includes('value:"playlist"') &&
@@ -42,7 +50,12 @@ Menus.Playlist = Object.values(require(playlistMenuChunkID)).find(
 
 export const Cards: any = Object.assign(
 	{
-		Generic: findBy("cardPlayButtonFactory", "featureIdentifier", "variant")(exportedFunctions),
+		Generic: exportedFunctions.find((f) =>
+			f.toString().includes("OnMouseDown") &&
+			f.toString().match(/^[^;]*headerText/) &&
+			f.toString().match(/^[^;]*featureIdentifier/) &&
+			f.toString().match(/^[^;]*renderCardImage/)
+		),
 		HeroGeneric: findBy(
 			"cardPlayButtonFactory",
 			"featureIdentifier",
@@ -78,12 +91,17 @@ export const Cards: any = Object.assign(
 	),
 );
 
-export const RemoteConfigProviderComponent: React.FC<any> = findBy("resolveSuspense", "configuration")(
+export const RemoteConfigProviderComponent: React.FC<any> = findBy(
+	"resolveSuspense",
+	"configuration",
+)(
 	exportedFunctions,
 );
 
 export const Slider: React.FC<any> = exportedFunctions.find(
-	(m) => m.toString().includes("onStepBackward") && !m.toString().includes("volume"),
+	(m) =>
+		m.toString().includes("onStepBackward") &&
+		!m.toString().includes("volume"),
 );
 
 const exportedMemoFRefs = exportedMemos.filter(
@@ -96,28 +114,54 @@ export const NavTo: React.NamedExoticComponent = exportedMemoFRefs.find((m) =>
 	(m as any).type.render.toString().includes("pageId")
 )!;
 
-export const InstrumentedRedirect: React.FC<any> = findBy("getInteractionId")(exportedFunctions);
+export const InstrumentedRedirect: React.FC<any> = findBy("getInteractionId")(
+	exportedFunctions,
+);
 
-export const SnackbarProvider: SnackbarProviderT = findBy("enqueueSnackbar called with invalid argument")(
+export const SnackbarProvider: SnackbarProviderT = findBy(
+	"enqueueSnackbar called with invalid argument",
+)(
 	exportedFunctions,
 ) as unknown as SnackbarProvider;
 
 export const ContextMenu: any = Object.values(require(ContextMenuModuleID))[0];
-export const RightClickMenu: React.FC<any> = findBy("action", "open", "trigger", "right-click")(
+export const RightClickMenu: React.FC<any> = findBy(
+	"action",
+	"open",
+	"trigger",
+	"right-click",
+)(
 	exportedFunctions,
 );
 
-export const ConfirmDialog: React.FC<any> = findBy("isOpen", "shouldCloseOnEsc", "onClose")(exportedFunctions);
-export const Tooltip: React.FC<any> = findBy("hover-or-focus", "tooltip")(exportedFunctions);
+export const ConfirmDialog: React.FC<any> = findBy(
+	"isOpen",
+	"shouldCloseOnEsc",
+	"onClose",
+)(exportedFunctions);
+export const Tooltip: React.FC<any> = findBy("hover-or-focus", "tooltip")(
+	exportedFunctions,
+);
 
-export const Menu: React.FC<any> = findBy("getInitialFocusElement", "children")(exportedFunctions);
-export const MenuItem: React.FC<any> = findBy("handleMouseEnter", "onClick")(exportedFunctions);
-export const MenuItemSubMenu: React.FC<any> = findBy("subMenuIcon")(exportedFunctions);
+export const Menu: React.FC<any> = findBy("getInitialFocusElement", "children")(
+	exportedFunctions,
+);
+export const MenuItem: React.FC<any> = findBy("handleMouseEnter", "onClick")(
+	exportedFunctions,
+);
+export const MenuItemSubMenu: React.FC<any> = findBy("subMenuIcon")(
+	exportedFunctions,
+);
 
 export const RemoteConfigProvider = ({
 	configuration = Platform.getRemoteConfiguration(),
 	children = undefined as React.ReactNode,
-}) => React.createElement(RemoteConfigProviderComponent, { configuration }, children);
+}) =>
+	React.createElement(
+		RemoteConfigProviderComponent,
+		{ configuration },
+		children,
+	);
 
 export const PanelHeader: React.FC<any> = exportedFunctions.find(
 	(m) => m.toString().includes("panel") && m.toString().includes("actions"),
@@ -125,7 +169,8 @@ export const PanelHeader: React.FC<any> = exportedFunctions.find(
 export const PanelContent: React.FC<any> = exportedForwardRefs.find((f) =>
 	f.render.toString().includes("fixedHeader")
 );
-export const PanelSkeleton: React.FC<any> = findBy("label", "aside")(exportedFunctions) ||
+export const PanelSkeleton: React.FC<any> =
+	findBy("label", "aside")(exportedFunctions) ||
 	findBy((m) => m.render.toString().includes("section"))(exportedForwardRefs);
 
 export const Snackbar = {
@@ -138,21 +183,36 @@ export const Snackbar = {
 export const FilterBox: React.NamedExoticComponent = exportedMemos.find((f) =>
 	(f as any).type.toString().includes("filterBoxApiRef")
 )!;
-export const ScrollableContainer: React.FC<any> = findBy("scrollLeft", "showButtons")(exportedFunctions);
-export const ScrollableText: React.FC<any> = findBy("scrollLeft", "pauseAtEndEdgeDurationMs")(
+export const ScrollableContainer: React.FC<any> = findBy(
+	"scrollLeft",
+	"showButtons",
+)(exportedFunctions);
+export const ScrollableText: React.FC<any> = findBy(
+	"scrollLeft",
+	"pauseAtEndEdgeDurationMs",
+)(
 	exportedFunctions,
 );
-export const Toggle: React.FC<any> = findBy("onSelected", 'type:"checkbox"')(exportedFunctions);
-export const Router: React.FC<any> = findBy("navigationType", "static")(exportedFunctions);
+export const Toggle: React.FC<any> = findBy("onSelected", 'type:"checkbox"')(
+	exportedFunctions,
+);
+export const Router: React.FC<any> = findBy("navigationType", "static")(
+	exportedFunctions,
+);
 export const Routes: React.FC<any> = findBy(
 	/\([a-zA-Z_\$][\w\$]*\)\{let\{children:[a-zA-Z_\$][\w\$]*,location:[a-zA-Z_\$][\w\$]*\}=[a-zA-Z_\$][\w\$]*/,
 )(exportedFunctions);
 export const Route: React.FC<any> = findBy(
 	/^function [a-zA-Z_\$][\w\$]*\([a-zA-Z_\$][\w\$]*\)\{\(0,[a-zA-Z_\$][\w\$]*\.[a-zA-Z_\$][\w\$]*\)\(\!1\)\}$/,
 )(exportedFunctions);
-export const StoreProvider: React.FC<any> = findBy("notifyNestedSubs", "serverState")(exportedFunctions);
+export const StoreProvider: React.FC<any> = findBy(
+	"notifyNestedSubs",
+	"serverState",
+)(exportedFunctions);
 
-export const GenericModal: React.FC<any> = findBy("isOpen", "contentLabel")(exportedFunctions);
+export const GenericModal: React.FC<any> = findBy("isOpen", "contentLabel")(
+	exportedFunctions,
+);
 
 export const Tracklist: React.FC<any> = exportedMemos.find((f) =>
 	(f as any).type.toString().includes("nrValidItems")
@@ -168,4 +228,6 @@ export const TracklistColumnsContextProvider: React.FC<any> = findBy(
 	exportedFunctions,
 );
 
-export const SettingsToggle: React.FC<any> = findBy("condensed", "onSelected")(exportedFunctions);
+export const SettingsToggle: React.FC<any> = findBy("condensed", "onSelected")(
+	exportedFunctions,
+);
