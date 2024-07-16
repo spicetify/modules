24e6996281493184b14b089225db5f96cdc1df44
Delusoire <deluso7re@outlook.com>
2024-07-16 21:58:31 +0100

    feat: allow module exports modifications

--- modules/stdlib/mix.ts
+++ modules/stdlib/mix.ts
@@ -5,6 +5,12 @@
 
 import type { Transformer } from "/hooks/index.ts";
 
+const nativeObjectDefineProperty = Object.defineProperty;
+Object.defineProperty = function (obj, prop, descriptor) {
+	prop !== "prototype" && descriptor && (descriptor.configurable ??= true);
+	return nativeObjectDefineProperty(obj, prop, descriptor);
+};
+
 export let transformer: Transformer;
 export default async function (t: Transformer) {
 	transformer = t;
