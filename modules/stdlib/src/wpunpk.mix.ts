e521dbca10970aaa1aa2400ac00a623868c02d43
Delusoire <deluso7re@outlook.com>
2024-08-29 23:30:53 +0100

    y

--- modules/stdlib/src/wpunpk.mix.ts
+++ modules/stdlib/src/wpunpk.mix.ts
@@ -1,5 +1,6 @@
 export type WebpackRequire = {
 	m: Chunk[1];
+	g: typeof globalThis;
 };
 
 export type Module = Function;
@@ -22,7 +23,43 @@ const webpackChunkclient_web = [[
 	{},
 	($: WebpackRequire) => {
 		globalThis["__webpack_require__"] = webpackRequire = $;
+		const globalThisShadow = createGlobalThisShadow();
+		$.g = globalThisShadow;
+
+		const $sendCosmosRequest = globalThis.sendCosmosRequest;
+		Object.defineProperty(globalThisShadow, "sendCosmosRequest", {
+			value: function ($) {
+				try {
+					const request = JSON.parse($.request);
+					const { uri, method } = request;
+					const body = JSON.parse(request.body);
+					const { type, height } = body;
+
+					if (
+						uri === "sp://messages/v1/container/control" &&
+						method === "POST" &&
+						type === "update_titlebar"
+					) {
+						console.error(height);
+					}
+				} catch (e) {}
+
+				return $sendCosmosRequest($);
+			},
+		});
 	},
 ] as Chunk];
 
 globalThis.webpackChunkclient_web = webpackChunkclient_web;
+
+function createGlobalThisShadow() {
+	const globalThisShadow = {} as typeof globalThis;
+
+	Object.setPrototypeOf(globalThisShadow, globalThis);
+
+	Object.defineProperty(globalThisShadow, "self", { value: globalThisShadow });
+	Object.defineProperty(globalThisShadow, "globalThis", { value: globalThisShadow });
+	Object.defineProperty(globalThisShadow, "window", { value: globalThisShadow });
+
+	return globalThisShadow;
+}
