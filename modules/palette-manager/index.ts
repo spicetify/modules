d8795bbaf173d86d9d7759d517c6d61c942db249
Delusoire <deluso7re@outlook.com>
2024-09-01 21:52:32 +0100

    y

--- modules/palette-manager/index.ts
+++ modules/palette-manager/index.ts
@@ -1,9 +1,9 @@
 import { IndexLoadFn, IndexPreloadFn } from "/hooks/module.ts";
 
-export const preload: IndexPreloadFn = async (tr) => {
-	return await (await import("./palette.ts")).default(tr);
+export const preload: IndexPreloadFn = async (context) => {
+	return await (await import("./palette.ts")).default(context.module);
 };
 
-export const load: IndexLoadFn = async (mod) => {
-	return await (await import("./mod.tsx")).default(mod);
+export const load: IndexLoadFn = async (context) => {
+	return await (await import("./mod.tsx")).default(context.module);
 };
